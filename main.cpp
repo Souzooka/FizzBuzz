c42be62cd8e208bd8f93321c3551915eae1195a8
Dakota <bazookaharwell@gmail.com>
2017-03-24 14:32:49 -1000

    fizzbuzz

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 void fizzBuzz(int range)
 {
-    ;
+    for (int i = 1; i <= range; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            cout << "Fizz Buzz" << endl;
+        }
+        else if (i % 3 == 0)
+        {
+            cout << "Fizz" << endl;
+        }
+        else if (i % 5 == 0)
+        {
+            cout << "Buzz" << endl;
+        }
+    }
 }
 
 int main()
