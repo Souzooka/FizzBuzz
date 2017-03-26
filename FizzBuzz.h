f39772fe0f9d7f39f237ba0eab76443a830c6266
Dakota <bazookaharwell@gmail.com>
2017-03-25 19:34:51 -1000

    header file with declaration of fizzBuzz

---
+++ FizzBuzz.h
@@ -0,0 +1,6 @@
+#ifndef FIZZBUZZ_H_INCLUDED
+#define FIZZBUZZ_H_INCLUDED
+
+void fizzBuzz(int range);
+
+#endif // FIZZBUZZ_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
+#include "FizzBuzz.h"
 
 using namespace std;
-
-void fizzBuzz(int range);
 
 int main()
 {
